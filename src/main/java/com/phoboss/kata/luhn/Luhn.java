167c756ccfc004f856396e997bc4b62a79b3329d
jmolina <jmolina@thoughtworks.com>
2017-03-21 14:50:26 +0100

    refator reverse

--- src/main/java/com/phoboss/kata/luhn/Luhn.java
+++ src/main/java/com/phoboss/kata/luhn/Luhn.java
@@ -1,16 +1,14 @@
 package com.phoboss.kata.luhn;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Created by jmolina on 21/3/17.
  */
 public class Luhn {
+
     public String reverse(String input) {
-        String output = "";
-
-        for (int i = input.length() - 1; i >= 0; i--) {
-            output += input.charAt(i);
-        }
-
-        return output;
+        return StringUtils.reverse(input);
     }
+
 }
