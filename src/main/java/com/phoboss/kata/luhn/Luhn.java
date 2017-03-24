7fc17d1e5269083b4e17f53dfd4758efe533ddde
jmolina <jmolina@thoughtworks.com>
2017-03-24 17:49:16 +0100

    Added test luhn and validate. Yayyygit add .

--- src/main/java/com/phoboss/kata/luhn/Luhn.java
+++ src/main/java/com/phoboss/kata/luhn/Luhn.java
@@ -9,24 +9,46 @@ import java.util.function.Function;
  */
 public class Luhn {
 
+    private CardNumberValidator cardNumberValidator;
+
+    public Luhn() {
+        this(new CardNumberValidator());
+    }
+
+    public Luhn(CardNumberValidator cardNumberValidator) {
+        this.cardNumberValidator = cardNumberValidator;
+    }
+
     public String reverse(String input) {
         return StringUtils.reverse(input);
     }
 
     public int sumOdd(String input) {
-        return getResult(input, Function.identity(), 1, 0);
+        return computeLuhn(input, Function.identity(), 1, 0);
     }
 
     public int sumEven(String input) {
-        return getResult(input, x -> x > 9 ? x - 9 : x, 2, 1);
+        return computeLuhn(input, x -> x > 9 ? x - 9 : x, 2, 1);
     }
 
-    private int getResult(String input, Function<Integer, Integer> function, Integer multiplier, Integer origin) {
+    private int computeLuhn(String input, Function<Integer, Integer> function, Integer multiplier, Integer origin) {
         int result = 0;
         for (int i = origin; i < input.length(); i += 2) {
             int valueOf = function.apply(Integer.valueOf(input.substring(i, i + 1)) * multiplier);
             result += valueOf;
         }
         return result;
+    }
+
+    public boolean validate(String input) {
+        boolean isInputValid = cardNumberValidator.validate(input);
+        if (isInputValid) {
+            String reverseInput = reverse(input);
+            int result = sumEven(reverseInput) + sumOdd(reverseInput);
+            return result % 10 == 0;
+        } else {
+            return false;
+        }
+
     }
 }
--- src/test/java/com/phoboss/kata/luhn/TestLuhn.java
+++ src/test/java/com/phoboss/kata/luhn/TestLuhn.java
@@ -4,6 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by jmolina on 21/3/17.
@@ -24,5 +26,23 @@ public class TestLuhn {
         String result = luhn.reverse(input);
 
         assertEquals("The reverse is not working", "!dlrow olleH", result);
+    }
+
+    @Test
+    public void testGivenCorrectCardNumberWhenLuhnExpectSuccess() throws Exception {
+        String input = "5227190382720519";
+
+        boolean luhnSuccess = luhn.validate(input);
+
+        assertTrue("Luhn test is not working", luhnSuccess);
+    }
+
+    @Test
+    public void testGivenInvalidCardNumberWhenLuhnExpectFailure() throws Exception {
+        String input = "patatabullida";
+
+        boolean luhnSuccess = luhn.validate(input);
+
+        assertFalse("Luhn test is not working", luhnSuccess);
     }
 }
