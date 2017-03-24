300cebf1af897eabd3a55639bdc02141a34a30ed
jmolina <jmolina@thoughtworks.com>
2017-03-24 17:22:58 +0100

    Added sumEven Refactor

--- src/main/java/com/phoboss/kata/luhn/Luhn.java
+++ src/main/java/com/phoboss/kata/luhn/Luhn.java
@@ -2,6 +2,8 @@ package com.phoboss.kata.luhn;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.function.Function;
+
 /**
  * Created by jmolina on 21/3/17.
  */
@@ -13,20 +15,39 @@ public class Luhn {
 
     public int sumOdd(String input) {
         int result = 0;
-        for (int i = 0; i < input.length(); i+=2) {
-            result += Integer.valueOf(input.substring(i, i+1));
+        for (int i = 0; i < input.length(); i += 2) {
+
+            Function f = new Function<Integer, Integer>() {
+                @Override
+                public Integer apply(Integer valueOf) {
+                    return valueOf;
+                }
+            };
+
+            int valueOf = getAnInt(input, i, 1, f);
+            result += valueOf;
         }
 
         return result;
     }
 
+    private int getAnInt(String input, int i, int multiplier, Function<Integer, Integer> f) {
+        return f.apply(Integer.valueOf(input.substring(i, i + 1)) * multiplier);
+    }
+
     public int sumEven(String input) {
         int result = 0;
-        for (int i = 1; i < input.length(); i+=2){
-            int valueOf = Integer.valueOf(input.substring(i, i+1))*2;
-            if(valueOf > 9)
-                valueOf = (valueOf - 10) + 1;
-
+        for (int i = 1; i < input.length(); i += 2) {
+            Function f = new Function<Integer, Integer>() {
+                @Override
+                public Integer apply(Integer valueOf) {
+                    if (valueOf > 9)
+                        return (valueOf - 10) + 1;
+                    else
+                        return valueOf;
+                }
+            };
+            int valueOf = getAnInt(input, i, 2, f);
             result += valueOf;
         }
 
